b78c5cca6f4cca0b2ce2ce860a23052183f604f1
Mamad Jadali <jadalimamad@gmail.com>
2025-05-12 00:48:57 +0330

    auth setup

---
+++ auth.config.ts
@@ -0,0 +1,5 @@
+import Google from "next-auth/providers/google"
+import Credentials from "next-auth/providers/credentials"
+import type { NextAuthConfig } from "next-auth"
+ 
+export default { providers: [Google,Credentials] } satisfies NextAuthConfig

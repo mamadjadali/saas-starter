5b5b3209d5a20136837cbd958913b1942de7c7b5
Mamad Jadali <jadalimamad@gmail.com>
2025-05-12 14:22:39 +0330

    not found

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,13 @@
+import { Badge } from '@/components/Badge';
+import { LogoStatic } from '@/public/LogoStatic';
 import Link from 'next/link';
-import { CircleIcon } from 'lucide-react';
 
 export default function NotFound() {
   return (
     <div className="flex items-center justify-center min-h-[100dvh]">
       <div className="max-w-md space-y-8 p-4 text-center">
         <div className="flex justify-center">
-          <CircleIcon className="size-12 text-orange-500" />
+          <LogoStatic className="size-[5rem] text-orange-500" />
         </div>
         <h1 className="text-4xl font-bold text-gray-900 tracking-tight">
           Page Not Found
@@ -15,9 +16,10 @@ export default function NotFound() {
           The page you are looking for might have been removed, had its name
           changed, or is temporarily unavailable.
         </p>
+        <Badge className='py-1 px-4' variant='error'>Status Code: 404</Badge>
         <Link
           href="/"
-          className="max-w-48 mx-auto flex justify-center py-2 px-4 border border-gray-300 rounded-full shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
+          className="max-w-48 mx-auto flex justify-center py-2 px-4 border border-gray-300 rounded-xl shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-orange-500"
         >
           Back to Home
         </Link>
